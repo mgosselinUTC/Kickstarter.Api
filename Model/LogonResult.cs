5ae13cb05e5003c46b3ce8e72fcbe70481620810
mgosselinUTC <mgosselin@utc4me.org>
2014-11-10 13:31:05 -0500

    things happened, not erroring, so thats cool

--- Model/LogonResult.cs
+++ Model/LogonResult.cs
@@ -8,7 +8,11 @@ namespace Kickstarter.Api.Model
         [DataMember(Name = "access_token")]
         public string AccessToken { get; set; }
 
+
         [DataMember(Name = "user")]
         public User User { get; set; }
+
+
+
     }
 }
